28dc420be28201a99e4cea0d9abd101e5f79e567
carlosaaronvgomez-sudo <carlosaaronvgomez@gmail.com>
2026-04-19 21:08:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,16 @@ client.on('messageCreate', message => {
       Classic: 'https://www.wowisclassic.com/en/best-in-slot/warrior/?phase=1&specialization=prot',
       TBC: 'https://wowtbc.gg/bis-list/protection-warrior/',
       LK: 'https://wowtbc.gg/wotlk/bis-list/protection-warrior/'
-    }
+     },
+    'gue-fur': {
+      Classic: 'https://www.wowisclassic.com/en/best-in-slot/warrior/?phase=1&specialization=fury',
+      TBC: 'https://wowtbc.gg/bis-list/fury-warrior/',
+      LK: 'https://wowtbc.gg/wotlk/bis-list/fury-warrior/'
+    },
+   'gue-arm': {
+      TBC: 'https://wowtbc.gg/bis-list/arms-warrior/',
+      LK: 'https://wowtbc.gg/wotlk/bis-list/arms-warrior/'
+    } 
   }; // ✅ AQUÍ SE CIERRA
 
   const comando = msg.replace('!', '');
@@ -76,16 +85,6 @@ client.on('messageCreate', message => {
     message.reply(respuesta);
   }
 });
-
-  'gue-fur': {
-    Classic: 'https://www.wowisclassic.com/en/best-in-slot/warrior/?phase=1&specialization=fury',
-    TBC: 'https://wowtbc.gg/bis-list/fury-warrior/',
-    LK: 'https://wowtbc.gg/wotlk/bis-list/fury-warrior/'
-  },
-  'gue-arm': {
-    TBC: 'https://wowtbc.gg/bis-list/arms-warrior/',
-    LK: 'https://wowtbc.gg/wotlk/bis-list/arms-warrior/'
-   
  },
     'cl-bru': 'https://www.wowisclassic.com/en/best-in-slot/warlock/?phase=1&specialization=warlock',
     'tbc-bru-afli': 'https://wowtbc.gg/bis-list/affliction-warlock/',
